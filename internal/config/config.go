57953b68eaeeaae80eefd317cf483c1bbc4026c0
Casen Nagy <casen.nagy@gmail.com>
2025-12-21 23:53:49 -0600

    did really nothing but at the same time something

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"os"
+	"encoding/json"
+	"path/filepath"
 )
 
 const configFileName = "/.gatorconfig.json"
@@ -11,7 +13,49 @@ type Config struct {
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func Read() {
-	home = os.UserHomeDir()
+func Read() (Config, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, err
+	}
+
+	path := filepath.Join(home, configFileName)
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var cfg Config
+	if err = json.Unmarshal(data, &cfg); err!=nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+
+}
+
+func (cfg *Config) SetUser(name string) error {
+
+	cfg.CurrentUserName = name
+
+	home, err := os.UserHomeDir()
+	if err !=nil {
+		return err
+	}
+
+	path := filepath.Join(home,  configFileName)
+
+	data, err := json.Marshal(*cfg)
+	if err!=nil {
+		return err
+	}
+
+	err = os.WriteFile(path, data, 0o644)
+	if err!=nil {
+		return err
+	}
+
+	return nil
 
 }
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/sharkbait0402/blog-aggregator/internal/config"
+	"fmt"
+)
+
 func main() {
+
+	cfg, err := config.Read()
+	if err!=nil {
+		fmt.Errorf("read unsuccessful")
+	}
+
+	cfg.SetUser("Casen")
+
+	cfg, err = config.Read()
+		if err!=nil {
+			fmt.Errorf("read unsuccessful")
+		}
+
+	fmt.Printf("%+v\n", cfg)
 
 }
